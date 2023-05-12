a72f03dbf2b3e34347842d0d8bccb386d34b65ae
Christophertechn <njengachris42@gmail.com>
2023-05-12 16:40:23 +0300

    grid assignment

---
+++ index.js
@@ -0,0 +1,103 @@
+
+//Question 1
+let name = "Mary"; 
+function greetPeople(){
+     let name = "Clause"; 
+     console.log(`Hello ${name}`)
+}
+     greetPeople()
+
+     //answer is Hello Clause
+
+
+//Question 3
+// let number= prompt("Enter your number");
+
+// if((number>=50)&&(number<=100)){
+// console.log(alert("True"));
+// }
+
+// else if ((number>=60)||(number<=100)){
+// console.log(alert("False"));
+// }
+// else{
+//     console.log(alert("invalid input"));
+// }
+
+
+
+
+
+// Question 4
+
+//   let david =80;
+//   let vintoh=77;
+//   let divya=88; 
+//   let ishitha=95;
+//   let thomas=68;
+//  average_max=((david+vintoh+divya+ishitha+thomas)/5);
+
+//  console.log(alert("This is your average marks of all the five of top students in class is "+average_max));
+// //   grade=average_max;
+
+//   if((grade<=59)&&(grade>=50)){
+//     console.log(alert("Your grade is F"))
+//   }
+//   else if((grade>=60)&&(grade<=69)){
+//     console.log(alert("Your grade is E"))
+//   }
+
+//     else if((grade>=70)&&(grade<=79)){
+//       console.log(alert("Your grade is D"))
+//     }
+
+//       else if((grade>=80)&&(grade<=89)){
+//         console.log(alert("Your grade is C"))
+//       }
+//       else if((grade>=90)&&(grade<=99)){
+//         console.log(alert("Your grade is B"))
+//       }
+
+//         else if((grade>=90)&&(grade<=100)){
+//           console.log(alert("Your grade is A"))
+//         }
+          
+//           else{
+//             console.log(alert("invalid input: The Grading scheme only runs from 50 to 100"))
+//           }
+
+
+
+
+// //Question 5
+// const color = ["Blue", "Green", "Red", "Pink"];
+// let o = ["th", "st", "nd", "rd", ];
+// for (let i = 0; i < color.length; i++) {
+//   if(color[i]=="Blue"){
+//     console.log(`${i+1}${o[1]} choice is ${color[i]}`);
+//   }
+//   else if(color[i]=="Green"){
+//     console.log(`${i+1}${o[2]} choice is ${color[i]}`);
+//   }
+//   else if(color[i]=="Red"){
+//     console.log(`${i+1}${o[3]} choice is ${color[i]}`);
+//   }
+//   else{ 
+//     console.log(`${i+1}${o[0]} choice is ${color[i]}`);
+// }
+//   }
+
+// //Question 3
+// const user={
+//     id: 339,
+//     name: 'Fred',
+//     age: 42,
+//     education: { degree: 'Masters'}
+// }
+
+
+// let {id, name, age, education} =user;
+// console.log(id, name, age, education);
+
+// const person ={...user};
+// console.log(person);
